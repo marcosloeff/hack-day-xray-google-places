cdf7a04539f2a02e8f95e1817bbf3e4b880e4843
Marcos Loeff <marcos.loeff@gmail.com>
2020-01-10 06:44:00 -0300

    Added pagetoken

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ const app = express();
 
 app.use('/', (req, res, next) => {
 	console.log('New request!');
-	let { lat, lng, type, radius, key } = req.query;
+	let { lat, lng, type, radius, key, pagetoken } = req.query;
 	if (lat) {
 		// if (!key) {
 		// 	key  = 'AIzaSyCARa0RvtRVxpc2YU3b2G8Uh0XC9bDHcxM';
 		// }
 		//res.status(200).json({ lat: lat });
-		const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + lat + ',' + lng + '&radius=' + radius 
+		let url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + lat + ',' + lng + '&radius=' + radius 
 		+ '&type=' + type + '&key=' + key;
+		if (pagetoken) {
+			url = url + '&pagetoken=' + pagetoken;
+		}
 		axios.get(url)
 			.then(response => {
 				res.status(200).json(response.data);
